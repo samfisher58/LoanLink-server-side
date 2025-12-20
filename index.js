80473d886c7bfd1759fd46055659fb624f6b98fc
samfisher58 <symonislam58@gmail.com>
2025-12-20 23:45:56 +0600

    make mongodb run function deploy ready

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		// await client.connect();
 
 		const db = client.db('Loan_link_db');
 		const loanCollection = db.collection('loans');
@@ -340,7 +340,7 @@ async function run() {
 		});
 
 		// Send a ping to confirm a successful connection
-		await client.db('admin').command({ ping: 1 });
+		// await client.db('admin').command({ ping: 1 });
 		console.log(
 			'Pinged your deployment. You successfully connected to MongoDB!'
 		);
